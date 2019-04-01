4ee0c89e6921a3a90a51c0328c2deca1413b944e
ajb85 <ajbrush85@gmail.com>
2019-04-01 17:09:43 -0400

    server.js setup.  Route structure created

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const helmet = require("helmet");
+const server = express();
+
+const registerRoutes = require("./register/regRoutes.js");
+const loginRoutes = require("./login/loginRoutes.js");
+const userRoutes = require("./users/userRoutes.js");
+
+server.use(express.json());
+server.use(helmet());
+
+server.get("/", (req, res) => {
+  res.status(200).send("Still alive");
+});
+
+server.use("/api/register", registerRoutes);
+server.use("/api/login", loginRoutes);
+server.use("/api/users", userRoutes);
+
+module.exports = server;
